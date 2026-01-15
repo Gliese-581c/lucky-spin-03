4a2cecd52f411c89b5e8a8c0819782ed5c0d3bac
Patrick <234474225+Gliese-581c@users.noreply.github.com>
2026-01-15 14:35:52 -0800

    Completes exercise step 2.

--- LuckySpin/Models/Spin.cs
+++ LuckySpin/Models/Spin.cs
@@ -8,14 +8,20 @@ public class Spin
     public string ImageDisplay { 
         get{
             //TODO: Add logic here to return either "block" or "none" based on whether the Numbers array contains the Luck value
-            return "";
+            return Numbers.Contains(Luck) ? "block" : "none";
         }
     }
     Random random = new Random();
     //TODO: Adjust Constructor to generate random Numbers for the Spin
+
     public Spin()
     {
-
+        int count = 3;
+        Numbers = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            Numbers[i] = random.Next(1, 10);
+        }
     }
 
 }
